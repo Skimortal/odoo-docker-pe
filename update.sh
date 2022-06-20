1af00015ca8377b033b754bad91a9ef53fbe2327
Skimortal <stojakovic.a@gmail.com>
2022-06-20 09:55:53 +0200

    Add script for updating git version of addons

---
+++ update.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+cd addons/paulundernst
+git pull
+cd ../../
+
+cd addons/crm_duplicates
+git pull
+cd ../../
+
+cd addons/partner_duplicates
+git pull
+cd ../../
+
+cd addons/scrimo_addons
+git pull
+cd ../../
+
+cd addons/scrimo_branding
+git pull
+cd ../../
+
+cd addons/scrimo_crm_automation
+git pull
+cd ../../
+
+cd addons/scrimo_crm_operation_stage
+git pull
+cd ../../
+
+cd addons/scrimo_crm_paulundernst
+git pull
+cd ../../
+
+cd addons/scrimo_layout
+git pull
+cd ../../
+
+cd addons/scrimo_mrp_employee
+git pull
+cd ../../
+
+cd addons/scrimo_multiprint
+git pull
+cd ../../
+
+cd addons/scrimo_partnername
+git pull
+cd ../../
+
+cd addons/scrimo_prepayment
+git pull
+cd ../../
+
+cd addons/scrimo_product_tags
+git pull
+cd ../../
+
+cd addons/scrimo_product_variant_change
+git pull
+cd ../../
+
+cd addons/scrimo_purchase
+git pull
+cd ../../
+
+cd addons/scrimo_sale_printdata
+git pull
+cd ../../
+
+cd addons/scrimo_translation_html
+git pull
+cd ../../
